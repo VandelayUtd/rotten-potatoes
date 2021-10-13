d3bf36aea905d2bfc3e5425afced5b2f39014834
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-13 10:42:57 -0400

    built out user new and show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+
+    def new 
+        @movie = Movie.new 
+    end
+
+    def create 
+        @movie = Movie.new(movie_params)
+        if @movie.save 
+            redirect_to movie_path(@movie)
+        else 
+            render "new"
+        end
+    end
+
+    def show 
+        @movie = Movie.find(params[:id])
+    end
+
+    private 
+
+    def movie_params 
+        params.require(:movie).permit(:title, :watched, :user_id )
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    # has_many :user_movies 
-    # has_many :movies, through: :user_movies
+    has_secure_password
     has_many :reviews
     has_many :movies 
     has_many :reviewed_movies, through: :reviews, source: :movie
+
+
 end
